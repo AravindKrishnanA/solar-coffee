326a6be951b89061e8594256776ca77c57aac131
Aravind Krishnan A <krishnanaravind8702@gmail.com>
2021-07-05 12:44:10 +0530

    serialised project view model

---
+++ SolarCoffee.Services/Product/ProductService.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SolarCoffee.Data;
+using System.Linq;
+using SolarCoffee.Data.Models;
+
+namespace SolarCoffee.Services.Product
+{
+    public class ProductService : IProductService
+    {
+
+        private readonly SolarDbContext _db;
+
+        public ProductService(SolarDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+        public ServiceResponse<Data.Models.Product> ArchiveProduct(int id)
+        {
+            try
+            {
+                var product = _db.Products.Find(id);
+                product.IsArchived = true;
+                _db.SaveChanges();
+
+                return new ServiceResponse<Data.Models.Product>
+                {
+                    Data = product,
+                    Time = DateTime.UtcNow,
+                    Message = "Archived product",
+                    IsSuccess = true
+                };
+            }
+
+            catch (Exception e)
+            {
+                return new ServiceResponse<Data.Models.Product>
+                {
+                    Data = null,
+                    Time = DateTime.UtcNow,
+                    Message = e.StackTrace,
+                    IsSuccess = false
+                };
+            }
+        }
+
+        public ServiceResponse<Data.Models.Product> CreateProduct(Data.Models.Product product)
+        {
+            try
+            {
+                _db.Products.Add(product);
+
+                var newInventory = new ProductInventory
+                {
+                    Product = product,
+                    QuantityOnHand = 0,
+                    IdealQuantity = 10
+                };
+
+                _db.ProductInventories.Add(newInventory);
+
+                _db.SaveChanges();
+                
+                return new ServiceResponse<Data.Models.Product>
+                {
+                    Data = product,
+                    Time = DateTime.UtcNow,
+                    Message = "Saved new Product",
+                    IsSuccess = true
+                };
+
+            }
+            
+            catch (Exception e)
+            {
+                return new ServiceResponse<Data.Models.Product>
+                {
+                    Data = product,
+                    Time = DateTime.UtcNow,
+                    Message = "Error saving new product",
+                    IsSuccess = false
+                };
+            }
+            
+        }
+
+        public List<Data.Models.Product> GetAllProducts()
+        {
+            return _db.Products.ToList();
+        }
+
+        public Data.Models.Product GetProductById(int id)
+        {
+            return _db.Products.Find(id);
+        }
+
+        ServiceResponse<Data.Models.Product> IProductService.ArchiveProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SolarCoffee.Web/Startup.cs
+++ SolarCoffee.Web/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using SolarCoffee.Data;
+using SolarCoffee.Services.Product;
 
 namespace SolarCoffee.Web
 {
@@ -36,6 +37,8 @@ namespace SolarCoffee.Web
                 opts.EnableDetailedErrors();
                 opts.UseNpgsql(Configuration.GetConnectionString("solar.dev"));
             });
+
+            services.AddTransient<IProductService, ProductService>();
             //services.AddSwaggerGen(c =>
             //{
             //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "SolarCoffee.Web", Version = "v1" });
